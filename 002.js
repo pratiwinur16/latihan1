b15fa5998fc1fa925661187c5a939851f243c1b4
pratiwinur16 <39262584+pratiwinur16@users.noreply.github.com>
2018-06-26 02:21:27 +0700

    Create 002.js

---
+++ 002.js
@@ -0,0 +1,50 @@
+/*
+==================================
+Array Mastery: Pair Up
+==================================
+Nama:________
+[INSTRUKSI]
+Setiap student di HACKTIV8 ditugaskan untuk membuat tim project dengan komposisi tim dua orang.
+Kita membutuhkan sebuah program untuk membuat tim-tim tersebut.
+Function pairUp akan menerima satu parameter berupa array yang berisi string nama student.
+Function ini akan mengelompokkan setiap dua nama menjadi satu kelompok, dan apabila tersisa satu student, student terakhir akan bersama instruktur.
+Hasil pengelompokkan adalah sebuah array yang berisi string berupa nama dua orang student sebagai satu tim.
+Formatnya adalah <Nama Student 1> dan <Nama Student 2>.
+
+[CONTOH]
+input: ['Budi', 'Badu', 'Indra', 'Indri']
+output: ['Budi dan Badu', 'Indra dan Indri']
+
+input: ['Budi', 'Badu', 'Indra', 'Indri', 'James']
+output: ['Budi dan Badu', 'Indra dan Indri', 'James dan Instruktur']
+
+input: []
+output: []
+
+[RULE]
+- Dilarang menggunakan Regex (.match, .test, dan sebagainya)
+- Dilarang menggunakan method .map, .filter, atau .reduce!
+
+*/
+
+function pairUp(students) {
+  var resultString = '';
+  var result = [];
+  
+  for(var i = 0 ; i < students.length ; i+=2){
+    if(students[i+1] !== undefined){                        // contoh var students ['Budi','Badu'] jika students[i] memiliki pasangan
+      resultString = students[i] + ' dan ' + students[i+1]
+    }else{
+      resultString = students[i] + ' dan Instruktur'
+    }
+    result.push(resultString)
+  }
+  return result
+}
+
+// TEST CASES
+console.log(pairUp(['Budi', 'Badu'])); // ['Budi dan Badu']
+console.log(pairUp(['Budi'])); // ['Budi dan Instruktur']
+console.log(pairUp(['Budi', 'Badu', 'Indra', 'Indri'])); // ['Budi dan Badu', 'Indra dan Indri']
+console.log(pairUp(['Budi', 'Badu', 'Indra', 'Indri', 'James'])); // ['Budi dan Badu', 'Indra dan Indri', 'James dan Instruktur']
+console.log(pairUp([])); // []
